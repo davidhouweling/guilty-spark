afa301cc8882af71460456f162c255880dc25546
David Houweling <davidhouweling@users.noreply.github.com>
2025-10-27 15:34:58 +1100

    establish routes and simplify

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -4,6 +4,15 @@
  */
 const config = {
   printWidth: 120,
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
 };
 
 export default config;
