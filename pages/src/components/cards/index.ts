3b2a399e56ab35b2e14d22bf0a77b047c9e58829
David Houweling <davidhouweling@users.noreply.github.com>
2025-11-05 10:46:03 +1100

    minor adjustments to cards usage

---
+++ pages/src/components/cards/index.ts
@@ -0,0 +1,4 @@
+export { default as BaseCard } from "./base-card.astro";
+export { default as CommandCard } from "./command-card.astro";
+export { default as FeatureCard } from "./feature-card.astro";
+export { default as StatCard } from "./stat-card.astro";
