b260951e2b197e5ee1982cb43400aa749c382f30
David Houweling <davidhouweling@users.noreply.github.com>
2025-11-05 13:54:15 +1100

    create demo card

--- pages/src/components/cards/index.ts
+++ pages/src/components/cards/index.ts
@@ -1,4 +1,5 @@
 export { default as BaseCard } from "./base-card.astro";
+export { default as DemoCard } from "./demo-card.astro";
 export { default as CommandCard } from "./command-card.astro";
 export { default as FeatureCard } from "./feature-card.astro";
 export { default as StatCard } from "./stat-card.astro";
