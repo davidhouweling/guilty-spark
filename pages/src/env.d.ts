e822f23053bcac054575651bb58e4387c4aaf637
David Houweling <houweling@outlook.com>
2025-12-13 22:35:44 +1100

    fix build

--- pages/src/env.d.ts
+++ pages/src/env.d.ts
@@ -4,6 +4,14 @@ declare namespace App {
   type Locals = Runtime<Env>;
 }
 
+interface ImportMetaEnv {
+  readonly PUBLIC_API_HOST?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare global {
   interface Window {
     __TRACKER_API_HOST__?: string;
