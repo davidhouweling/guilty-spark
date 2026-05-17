46ee47fc5f400d6b0ea33fdfbbf1e0b901be78f5
David Houweling <houweling@outlook.com>
2026-05-17 16:59:24 +1000

    fix: remove unsupported platformProxy from astro.config

--- pages/astro.config.ts
+++ pages/astro.config.ts
@@ -5,9 +5,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
     imageService: "cloudflare",
   }),
   integrations: [react()],
