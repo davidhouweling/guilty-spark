7872d83edd5a463bc6f901f3f7cc7fee2b857f62
David Houweling <houweling@outlook.com>
2026-06-10 09:09:52 +1000

    Improve cloudflare logging (#529)

--- api/services/log/console-log-client.ts
+++ api/services/log/console-log-client.ts
@@ -7,18 +7,36 @@ export class ConsoleLogClient implements LogService {
   }
 
   info(error: Error | string, extra?: ReadonlyMap<string, JsonAny>): void {
-    console.info(error, extra ? JSON.stringify([...extra], null, 2) : undefined);
+    console.info(this.format(error, extra));
   }
 
   warn(error: Error | string, extra?: ReadonlyMap<string, JsonAny>): void {
-    console.warn(error, extra ? JSON.stringify([...extra], null, 2) : undefined);
+    console.warn(this.format(error, extra));
   }
 
   error(error: Error | string, extra?: ReadonlyMap<string, JsonAny>): void {
-    console.error(error, extra ? JSON.stringify([...extra], null, 2) : undefined);
+    console.error(this.format(error, extra));
   }
 
   fatal(error: Error | string, extra?: ReadonlyMap<string, JsonAny>): void {
-    console.error("FATAL:", error, extra ? JSON.stringify([...extra], null, 2) : undefined);
+    console.error("FATAL:", this.format(error, extra));
+  }
+
+  private format(error: Error | string, extra?: ReadonlyMap<string, JsonAny>): string {
+    const content: Record<string, JsonAny> = {
+      message: typeof error === "string" ? error : error.message,
+    };
+
+    if (typeof error !== "string") {
+      content["stack"] = error.stack;
+    }
+
+    if (extra) {
+      for (const [key, value] of extra) {
+        content[key] = value;
+      }
+    }
+
+    return JSON.stringify(content, null, 2);
   }
 }
