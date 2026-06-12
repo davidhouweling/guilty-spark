fdce9ed6b69427d6105194e0e8411c14e3da2535
David Houweling <houweling@outlook.com>
2026-06-12 13:09:30 +1000

    Align halo film parser constants with extractor

--- api/services/halo/constants.ts
+++ api/services/halo/constants.ts
@@ -1,12 +1,12 @@
 export const HALO_PC_USER_AGENT =
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) HaloInfinite/6.10024.10518.0 Safari/537.36";
+  "SHIVA-2043073184/6.10021.18539.0 (release; PC)";
 
-export const MIN_XUID = 2533274790395904n;
-export const MAX_XUID = 2533274896535552n;
+export const MIN_XUID = 2_000_000_000_000_000n;
+export const MAX_XUID = 3_000_000_000_000_000n;
 
-export const EVENT_WINDOW_BITS = 704;
-export const EVENT_ENVELOPE_BYTES = 64;
-export const EVENT_TERMINATOR_BYTES = Uint8Array.from([0x9f, 0x69, 0x01]);
+export const EVENT_WINDOW_BITS = 20_000;
+export const EVENT_ENVELOPE_BYTES = 60;
+export const EVENT_TERMINATOR_BYTES = Uint8Array.of(0x00, 0x00, 0x2e, 0xe0);
 
 export const KILL_HINT = 50;
 export const DEATH_HINT = 20;
@@ -17,8 +17,6 @@ export const KILL_DEATH_PAIRING_MAX_DELTA_MS = 1;
 export const PERFECT_MEDAL_NAME_ID = 1512363953;
 export const PERFECTION_MEDAL_NAME_ID = 865763896;
 
-export const MEDAL_SORTING_WEIGHTS = new Map<number, number>([
-  [210, 100],
-  [220, 90],
-  [230, 80],
+export const MEDAL_SORTING_WEIGHTS = new Set<number>([
+  50, 51, 52, 100, 101, 150, 200, 205, 210, 220, 225, 230, 235, 240, 245, 250,
 ]);
