1cebebaa9b737bc97dda7475d144db8d38b481e1
David Houweling <houweling@outlook.com>
2026-07-06 22:32:00 +1000

    Revert "IndividualTrackerViewService: fix path post url change" (#654)

--- pages/src/services/individual-tracker/view.ts
+++ pages/src/services/individual-tracker/view.ts
@@ -39,7 +39,7 @@ export class RealIndividualTrackerViewService implements IndividualTrackerViewSe
   }
 
   public async getView(trackerId: string): Promise<TrackerViewResponse> {
-    const response = await fetch(this.buildUrl(`/api/individual-tracker/${encodeURIComponent(trackerId)}`), {
+    const response = await fetch(this.buildUrl(`/api/individual-tracker/${encodeURIComponent(trackerId)}/view`), {
       method: "GET",
     });
 
