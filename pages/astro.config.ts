cbdf589d61ff072a02ca9e5fc93dbc6a515ef155
David Houweling <houweling@outlook.com>
2026-07-12 19:20:30 +1000

    feat(pages): add sitemap generation (P2-01)

--- pages/astro.config.ts
+++ pages/astro.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://guilty-spark.app",
   adapter: cloudflare(),
-  integrations: [react()],
+  integrations: [
+    react(),
+    sitemap({
+      filter: (page) =>
+        ![
+          "https://guilty-spark.app/login",
+          "https://guilty-spark.app/tracker",
+          "https://guilty-spark.app/404",
+          "https://guilty-spark.app/500",
+        ].includes(page) &&
+        !page.startsWith("https://guilty-spark.app/individual-tracker/") &&
+        !page.startsWith("https://guilty-spark.app/stats/") &&
+        !page.startsWith("https://guilty-spark.app/u/"),
+    }),
+  ],
   scopedStyleStrategy: "class",
   trailingSlash: "never",
   vite: {
