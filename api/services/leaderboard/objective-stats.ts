49582b4c28f2cc121bf4f997a22a99cede811b79
David Houweling <houweling@outlook.com>
2026-08-24 13:48:13 +1000

    format

--- api/services/leaderboard/objective-stats.ts
+++ api/services/leaderboard/objective-stats.ts
@@ -3,8 +3,6 @@ import type { MatchStats } from "halo-infinite-api";
 type PlayerStats = MatchStats["Players"][number]["PlayerTeamStats"][number]["Stats"];
 
 export function serializeObjectiveStats(stats: PlayerStats): string {
-  const objectiveStats = Object.fromEntries(
-    Object.entries(stats).filter(([key]) => key !== "CoreStats"),
-  );
+  const objectiveStats = Object.fromEntries(Object.entries(stats).filter(([key]) => key !== "CoreStats"));
   return JSON.stringify(objectiveStats);
 }
